c85e2cd629ab552b4d4a08f907727f1bfffc7725
Rexalez <129522029+Rexalez@users.noreply.github.com>
2023-04-30 00:06:56 +0800

    Added User Registration function

--- js/register.js
+++ js/register.js
@@ -8,10 +8,47 @@ var firebaseConfig = {
     messagingSenderId: "872723654241",
     appId: "1:872723654241:web:f9f693c7c18d6d31e2ac20"
 };
-    
-    
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Initialize variables
 const auth = firebase.auth();
+const emailField = document.getElementById('exampleInputEmail3');
+const passwordField = document.getElementById('exampleInputPassword3');
+
+function register () {
+    const email = emailField.value;
+    const password = passwordField.value;
+    
+    // Check that email and password fields are not empty
+    if (!email || !password) {
+        alert('Please enter your email and password.');
+        return;
+    }
+    
+    // Check that email is in a valid format using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+    
+    // Create a new user account using Firebase Authentication
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // User account created successfully
+            const user = userCredential.user;
+            console.log('User account created:', user);
+            alert('User Created!');
+            window.location.assign("/html/login.html");
+            // TODO: You can add any further actions you want to take after a user account is created
+        })
+        .catch((error) => {
+            // Handle errors that occurred during account creation
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error('Error creating user account:', errorCode, errorMessage);
+            // TODO: You can add any further error handling you want to do here
+        });
+}
