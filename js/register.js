2ab959f4d317cd132be1c9329560ceb61d6f97e2
Rexalez <129522029+Rexalez@users.noreply.github.com>
2023-04-29 23:31:22 +0800

    .

---
+++ js/register.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD3RSWM_wDt8uq9mgHQfXn27ny5gXOf8og",
+    authDomain: "moonstreet-c0100.firebaseapp.com",
+    databaseURL: "https://moonstreet-c0100-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "moonstreet-c0100",
+    storageBucket: "moonstreet-c0100.appspot.com",
+    messagingSenderId: "872723654241",
+    appId: "1:872723654241:web:f9f693c7c18d6d31e2ac20"
+};
+    
+    
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize variables
+const auth = firebase.auth();
