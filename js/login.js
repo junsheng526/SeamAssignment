593263192cb0c6807530380178bf971dff66da64
junsheng526 <junshengchoo57@gmail.com>
2023-05-01 03:21:58 +0800

    update login and sign up ui

--- js/login.js
+++ js/login.js
@@ -1,67 +1,87 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyD3RSWM_wDt8uq9mgHQfXn27ny5gXOf8og",
-    authDomain: "moonstreet-c0100.firebaseapp.com",
-    databaseURL: "https://moonstreet-c0100-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "moonstreet-c0100",
-    storageBucket: "moonstreet-c0100.appspot.com",
-    messagingSenderId: "872723654241",
-    appId: "1:872723654241:web:f9f693c7c18d6d31e2ac20"
+  apiKey: "AIzaSyD3RSWM_wDt8uq9mgHQfXn27ny5gXOf8og",
+  authDomain: "moonstreet-c0100.firebaseapp.com",
+  databaseURL: "https://moonstreet-c0100-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "moonstreet-c0100",
+  storageBucket: "moonstreet-c0100.appspot.com",
+  messagingSenderId: "872723654241",
+  appId: "1:872723654241:web:f9f693c7c18d6d31e2ac20"
 };
-    
-    
+
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Initialize variables
 const auth = firebase.auth();
 
 // Set up our login function
-function login () {
-    // Get all our input fields
-    email = document.getElementById('exampleInputEmail1').value;
-    password = document.getElementById('exampleInputPassword1').value;
-  
-    // Validate input fields
-    if (validate_email(email) == false || validate_password(password) == false) {
-      alert('Email or Password is Outta Line!!');
-      return;
-      // Don't continue running the code
-    }
-  
-    auth.signInWithEmailAndPassword(email, password)
-    .then(function() {
-  
-      // DOne
+function login() {
+  // Get all our input fields
+  email = document.getElementById('email').value;
+  password = document.getElementById('password').value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert('Please fill in your email and password!');
+    return;
+    // Don't continue running the code
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(function () {
+
+      // Done
       window.location.assign("/index.html");
-      alert('User Logged In!!');
+      alert('Log in success as ' + email + '!');
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Firebase will use this to alert of its errors
       var error_code = error.code;
       var error_message = error.message;
-  
+
       alert(error_message);
     })
-  
-  }
 
-   // Validate Functions
-function validate_email(email) {
-expression = /^[^@]+@\w+(\.\w+)+\w$/;
-if (expression.test(email) == true) {
-  // Email is good
-  return true;
-} else {
-  // Email is not good
-  return false;
 }
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true;
+  } else {
+    // Email is not good
+    return false;
+  }
 }
 
 function validate_password(password) {
-// Firebase only accepts lengths greater than 6
-if (password < 6) {
-  return false;
-} else {
-  return true;
-}
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false;
+  } else {
+    return true;
+  }
 }
